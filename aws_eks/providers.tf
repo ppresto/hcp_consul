81472a347633b8f499eefdf58584ba19818ff708
ppresto <ppresto@hashicorp.com>
2022-02-09 15:38:19 -0800

    1

--- aws_eks/providers.tf
+++ aws_eks/providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.74.1"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.7.0"
+    }
   }
 }
 
@@ -13,5 +17,4 @@ provider "kubernetes" {
   host                    = data.aws_eks_cluster.cluster.endpoint
   token                   = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate  = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  load_config_file        = false
 }
