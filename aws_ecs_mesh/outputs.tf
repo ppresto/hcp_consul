1fff227437e71c4aa4de8b89b96c0ef2bf5b310b
ppresto <ppresto@hashicorp.com>
2022-02-09 16:56:23 -0800

    1

--- aws_ecs_mesh/outputs.tf
+++ aws_ecs_mesh/outputs.tf
@@ -9,7 +9,3 @@ output "gossip_key" {
 output "consul_server_private_endpoint" {
   value = substr(local.consul_server_http_addr, 8, -1)
 }
-
-output "consul_retry_join" {
-  value = local.consul_retry_join
-}
--- aws_eks/outputs.tf
+++ aws_eks/outputs.tf
@@ -165,3 +165,7 @@ output "aws_auth_configmap_yaml" {
   description = "Formatted yaml output for base aws-auth configmap containing roles used in cluster node groups/fargate profiles"
   value       = module.eks.aws_auth_configmap_yaml
 }
+
+output "consul_retry_join" {
+  value = local.consul_retry_join
+}
