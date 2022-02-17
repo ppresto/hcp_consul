67e84641e19324be4a51d99debfdfa945f8d7e8a
ppresto <ppresto@hashicorp.com>
2022-02-17 12:15:12 -0800

    add intensions

--- aws_eks/4-ingress_gw.tf
+++ aws_eks/4-ingress_gw.tf
@@ -1,8 +1,15 @@
 data "kubectl_file_documents" "ingress_gw" {
     content = file("${path.module}/templates/ingress-gateway.yaml")
 }
+data "kubectl_file_documents" "svc_intensions" {
+    content = file("${path.module}/templates/service-intentions.yaml")
+}
 
 resource "kubectl_manifest" "reg_ingress_gw" {
     for_each  = data.kubectl_file_documents.ingress_gw.manifests
+    yaml_body = each.value
+}
+resource "kubectl_manifest" "apply_svc_intensions" {
+    for_each  = data.kubectl_file_documents.svc_intensions.manifests
     yaml_body = each.value
 }
