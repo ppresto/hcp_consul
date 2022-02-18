4255241cc8ebb1e94417f8ef28724d2d5efd7055
ppresto <ppresto@hashicorp.com>
2022-02-18 10:28:26 -0800

    added comment

--- aws_eks/2-hcp.tf
+++ aws_eks/2-hcp.tf
@@ -47,7 +47,16 @@ resource "helm_release" "consul" {
 
   repository = "https://helm.releases.hashicorp.com"
   chart      = "consul"
-  #version    = "0.32.1"
+  /*
+  version = "0.32.1" - Causes connect-inject 403 (Permission denied)
+    $  kubectl logs frontend-5d8bbf496-ql26d consul-connect-inject-init
+    2022-02-17T21:40:04.732Z [INFO]  Consul login complete
+    2022-02-17T21:40:04.735Z [INFO]  Registered service has been detected: service=frontend
+    2022-02-17T21:40:04.735Z [INFO]  Registered service has been detected: service=frontend-sidecar-proxy
+    2022-02-17T21:40:04.736Z [INFO]  Connect initialization completed
+    ==> Failed to create configuration to apply traffic redirection rules: failed to fetch proxy service from Consul Agent: Unexpected response code: 403 (Permission denied)
+  Fix: version = 0.33.0
+  */
   version    = "0.33.0"
 
   values = [data.template_file.agent_config.rendered]
