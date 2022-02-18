40ae8fdc6cbb4e2c45a4c2026bf6f9e6cd4c0b80
ppresto <ppresto@hashicorp.com>
2022-02-18 14:30:26 -0800

    add k8s provider

--- aws_eks/providers.tf
+++ aws_eks/providers.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.74.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.7.0"
+    }
   }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
