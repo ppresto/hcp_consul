de49f3bef57adee92004f15dd7b28e489a3bae31
ppresto <ppresto@hashicorp.com>
2022-02-22 15:03:15 -0800

    add url output

--- aws_eks_apps/outputs.tf
+++ aws_eks_apps/outputs.tf
@@ -5,3 +5,7 @@ output "consul_retry_join" {
 output "consul_config_yaml" {
   value = data.template_file.agent_config.rendered
 }
+
+output "url" {
+  value = value = "http://${data.kubernetes_service.ingress.status[0].load_balancer[0].ingress[0].hostname}:8080"
+}
