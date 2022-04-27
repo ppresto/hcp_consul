a72b1ac8e6048927fc32bec8e9595cd1209c7e1f
ppresto <ppresto@hashicorp.com>
2022-04-27 10:47:48 -0700

    5

--- tfcb_workspaces/multispace_run.tf
+++ tfcb_workspaces/multispace_run.tf
@@ -1,18 +1,4 @@
-resource "multispace_run" "hcp_consul" {
-  # Use string workspace names here and not data sources so that
-  # you can define the multispace runs before the workspace even exists.
-  organization = "presto-projects"
-  workspace    = "hcp_consul"
-}
-
 resource "multispace_run" "aws_network" {
   workspace    = "aws_network"
   organization = var.organization
-  depends_on   = [multispace_run.hcp_consul]
-}
-
-resource "multispace_run" "root" {
-  organization = "my-org"
-  workspace    = "tfc"
-  manual_confirm = true
 }
