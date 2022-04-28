034321b661a578d04b2213dece8d627e67c9b450
ppresto <ppresto@hashicorp.com>
2022-04-28 12:32:21 -0700

    fixed network ds

--- aws_eks_apps/data.tf
+++ aws_eks_apps/data.tf
@@ -1,13 +1,3 @@
-data "terraform_remote_state" "aws_network" {
-  backend = "remote"
-  config = {
-    organization = "presto-projects"
-    workspaces = {
-      name = "aws_network"
-    }
-  }
-}
-
 data "terraform_remote_state" "hcp_consul" {
   backend = "remote"
   config = {
