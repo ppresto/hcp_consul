30a3bfeaca8a6d641066341006e5c079f20a22c5
ppresto <ppresto@hashicorp.com>
2022-04-28 14:03:06 -0700

    2

--- aws_eks_apps/providers.tf
+++ aws_eks_apps/providers.tf
@@ -22,21 +22,21 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = data.aws-eks_cluster.cluster.endpoint
-  token                  = data.aws-eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws-eks_cluster.cluster.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws-eks_cluster.cluster.endpoint
-    token                  = data.aws-eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(data.aws-eks_cluster.cluster.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   }
 }
 provider "kubectl" {
-  host                   = data.aws-eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws-eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws-eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
