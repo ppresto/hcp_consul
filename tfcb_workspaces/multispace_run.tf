644073a4e810180314785dce3c611ef6c2450acd
ppresto <ppresto@hashicorp.com>
2022-04-28 10:04:38 -0700

    test multi-run

--- tfcb_workspaces/multispace_run.tf
+++ tfcb_workspaces/multispace_run.tf
@@ -1,12 +1,12 @@
-resource "multispace_run" "hcp_consul" {
+#resource "multispace_run" "hcp_consul" {
   # Use string workspace names here and not data sources so that
   # you can define the multispace runs before the workspace even exists.
-  organization = var.organization
-  workspace    = "hcp_consul"
-}
+#  organization = var.organization
+#  workspace    = "hcp_consul"
+#}
 
 resource "multispace_run" "aws_network" {
-  workspace    = "aws_network"
+  workspace    = "aws-tgw"
   organization = var.organization
-  depends_on   = [multispace_run.hcp_consul]
+  #depends_on   = [multispace_run.hcp_consul]
 }
