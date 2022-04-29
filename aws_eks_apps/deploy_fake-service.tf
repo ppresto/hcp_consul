6d55567ab22e1f1217df7b71854e016429cf91ff
ppresto <ppresto@hashicorp.com>
2022-04-29 14:05:05 -0700

    fs on eks

---
+++ aws_eks_apps/deploy_fake-service.tf
@@ -0,0 +1,16 @@
+data "kubectl_path_documents" "fake-service-yaml" {
+  pattern = "${path.module}/fake-service/*.yaml"
+}
+
+resource "kubectl_manifest" "fake-service" {
+    for_each  = toset(data.kubectl_path_documents.fake-service-yaml.documents)
+    yaml_body = each.value
+    depends_on = [helm_release.consul]
+}
+
+data "kubernetes_service" "ingress" {
+  metadata {
+    name = "consul-ingress-gateway"
+  }
+  depends_on = [kubectl_manifest.fake-service]
+}
