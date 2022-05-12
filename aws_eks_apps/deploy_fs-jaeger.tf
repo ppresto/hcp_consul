03999cf718a50c32534b3b6712a971694f5905cd
ppresto <ppresto@hashicorp.com>
2022-05-12 12:25:20 -0700

    fix fs

---
+++ aws_eks_apps/deploy_fs-jaeger.tf
@@ -0,0 +1,25 @@
+data "kubectl_path_documents" "fake-service-yaml" {
+  pattern = "${path.module}/fs-jaeger/*.yaml"
+}
+data "kubectl_path_documents" "fs-init" {
+  pattern = "${path.module}/fs-jaeger/init-consul-config/*.yaml"
+}
+
+resource "kubectl_manifest" "fs-init" {
+  for_each   = toset(data.kubectl_path_documents.fs-init.documents)
+  yaml_body  = each.value
+  depends_on = [helm_release.consul]
+}
+resource "kubectl_manifest" "fake-service" {
+  for_each   = toset(data.kubectl_path_documents.fake-service-yam.documents)
+  yaml_body  = each.value
+  depends_on = [kubectl_manifest.fs-init]
+}
+
+data "kubernetes_service" "ingress" {
+  metadata {
+    name = "consul-ingress-gateway"
+    namespace = var.namespace
+  }
+  depends_on = [kubectl_manifest.fake-service]
+}
