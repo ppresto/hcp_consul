7311d3887e14258cc7d3a0d85c88684a45d6137d
ppresto <ppresto@hashicorp.com>
2022-05-12 15:43:23 -0700

    2

--- aws_eks_apps/outputs.tf
+++ aws_eks_apps/outputs.tf
@@ -6,7 +6,7 @@ output "consul_config_yaml" {
   value = data.template_file.agent_config.rendered
 }
 
-output "url" {
-  value = "http://${data.kubernetes_service.ingress.status[0].load_balancer[0].ingress[0].hostname}:8080"
-  depends_on = [data.kubernetes_service.ingress]
-}
+#output "url" {
+#  value = "http://${data.kubernetes_service.ingress.status[0].load_balancer[0].ingress[0].hostname}:8080"
+#  depends_on = [data.kubernetes_service.ingress]
+#}
