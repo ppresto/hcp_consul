a87a8ae1623eefa3bdaaaee9d3fc92f6b6b89aa7
ppresto <ppresto@hashicorp.com>
2022-05-17 16:16:35 -0700

    check namespace exist

--- aws_eks_apps/helm.tf
+++ aws_eks_apps/helm.tf
@@ -17,23 +17,65 @@ resource "helm_release" "consul" {
   create_namespace = true
   repository = "https://helm.releases.hashicorp.com"
   chart      = "consul"
-  /*
-  version = "0.32.1" - Causes connect-inject 403 (Permission denied)
-    $  kubectl logs frontend-5d8bbf496-ql26d consul-connect-inject-init
-    2022-02-17T21:40:04.732Z [INFO]  Consul login complete
-    2022-02-17T21:40:04.735Z [INFO]  Registered service has been detected: service=frontend
-    2022-02-17T21:40:04.735Z [INFO]  Registered service has been detected: service=frontend-sidecar-proxy
-    2022-02-17T21:40:04.736Z [INFO]  Connect initialization completed
-    ==> Failed to create configuration to apply traffic redirection rules: failed to fetch proxy service from Consul Agent: Unexpected response code: 403 (Permission denied)
-  Fix: version = 0.33.0
-  */
   version = "0.33.0"
 
   values = [data.template_file.agent_config.rendered]
-
   set {
     name  = "global.image"
     value = "hashicorp/consul-enterprise:1.11.0-ent"
     #value = "hashicorp/consul:1.10.1"
+  }
+}
+
+#
+### Namespace
+#
+data "kubernetes_namespace" "check" {
+  metadata {
+    name = var.namespace
+  }
+}
+#output "ns-present" {
+#  value = lookup(data.kubernetes_namespace.testns, "id") != null
+#}
+resource "kubernetes_namespace" "example" {
+  count = lookup(data.kubernetes_namespace.testns, "id") != null ? lookup(data.kubernetes_namespace.testns, "id") : 0
+  metadata {
+    labels = {
+      service = "consul"
+    }
+    name = "consul"
+  }
+}
+#
+### Configure Consul Secrets for the Helm Chart
+#
+resource "kubernetes_secret" "consul-ca-cert" {
+  metadata {
+    name = "consul-ca-cert"
+    namespace = var.namespace
+  }
+  data = {
+    "tls.crt" = base64decode(data.terraform_remote_state.hcp_consul.outputs.consul_ca_file)
+  }
+}
+
+resource "kubernetes_secret" "consul-gossip-key" {
+  metadata {
+    name = "consul-gossip-key"
+    namespace = var.namespace
+  }
+  data = {
+    "key" = local.consul_config_file.encrypt
+  }
+}
+
+resource "kubernetes_secret" "consul-bootstrap-token" {
+  metadata {
+    name = "consul-bootstrap-token"
+    namespace = var.namespace
+  }
+  data = {
+    "token" = local.consul_acl_token
   }
 }
