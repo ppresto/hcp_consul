5712caba93fc0b5cd9802a23acabfeabdee112bd
ppresto <ppresto@hashicorp.com>
2022-05-18 08:55:13 -0700

    create consul ns

--- aws-eks/eks-ns.tf
+++ aws-eks/eks-ns.tf
@@ -1,10 +1,8 @@
 #
 ### Create K8s Namespace if it doesn't exist
 #
-data "kubernetes_all_namespaces" "allns" {}
 
 resource "kubernetes_namespace" "create" {
-  count = contains(data.kubernetes_all_namespaces.allns.namespaces, "consul") ? 0 : 1
   metadata {
     labels = {
       service = "consul"
