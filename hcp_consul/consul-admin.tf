889795228fc2bfd37fd5ddec8b9bb25abc8d57e9
ppresto <ppresto@hashicorp.com>
2022-05-24 08:34:19 -0700

    2

--- hcp_consul/consul-admin.tf
+++ hcp_consul/consul-admin.tf
@@ -1,23 +1,16 @@
-#
-### Configure Consul
-#
-
-# Admin Partitions
+# Admin Partitions and Namespaces
 resource "consul_admin_partition" "qa" {
   name        = "qa"
   description = "Partition for QA Environment"
 }
-
 resource "consul_namespace" "qa-app-api" {
   name        = "api"
   description = "API App Team"
   partition   = consul_admin_partition.qa.name
-
   meta = {
     foo = "bar"
   }
 }
-
 resource "consul_namespace" "default-app-api" {
   name        = "api"
   description = "API App Team"
@@ -28,6 +21,7 @@ resource "consul_namespace" "default-app-api" {
   }
 }
 
+# Service Policies and Tokens (api)
 resource "consul_acl_policy" "api-service" {
   name        = "api-service"
   datacenters = ["dc1"]
@@ -60,9 +54,10 @@ data "consul_acl_token_secret_id" "api-service" {
     accessor_id = consul_acl_token.api-service.id
     pgp_key     = "keybase:my_username"
 }
-output "consul_acl_token_api_service" {
-  value = data.consul_acl_token_secret_id.api-service.secret_id
+output "consul_service_api_token" {
+  value = nonsensitive(data.consul_acl_token_secret_id.api-service.secret_id)
 }
+
 
 data "consul_autopilot_health" "read" {}
 output "autopilot_health" {
