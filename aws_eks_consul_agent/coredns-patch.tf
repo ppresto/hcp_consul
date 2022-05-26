e1e974aa7993d59636e9c3e5d4f3c7dcaed36711
ppresto <ppresto@hashicorp.com>
2022-05-26 14:48:51 -0700

    patch coredns

--- aws_eks_consul_agent/coredns-patch.tf
+++ aws_eks_consul_agent/coredns-patch.tf
@@ -1,5 +1,12 @@
+data "template_file" "coredns_configmap_patch" {
+  template = file("${path.module}/templates/coredns/coredns_patch.yaml")
+  vars = {
+    CONSUL_DNS_CLUSTER_IP = var.consul_dns_cluster_ip
+  }
+}
+
 data "kubectl_path_documents" "coredns-patch" {
-  pattern = "${path.module}/templates/coredns/coredns-patch.yaml"
+  pattern = "[data.template_file.coredns_configmap_patch.rendered]"
 }
 
 # Get consul dns server IP
