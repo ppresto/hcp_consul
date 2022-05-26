d88d41b5e138f7237b105589b0e3643ae01bf839
ppresto <ppresto@hashicorp.com>
2022-05-26 11:35:58 -0700

    3

--- aws-eks/ec2-node.tf
+++ aws-eks/ec2-node.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "node" {
   key_name                    = var.ec2_key_pair_name
   vpc_security_group_ids      = [aws_security_group.ec2-svc-node.id, data.terraform_remote_state.aws-tgw.outputs.consul_server_sg_id]
   subnet_id                   = data.terraform_remote_state.hcp_consul.outputs.vpc_private_subnets[0]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   user_data                   = data.template_file.userdata.rendered
   tags = merge(
     { "Name" = "presto-${var.region}-svc-node" },
