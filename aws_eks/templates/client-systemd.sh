6c0ec4babebec8334a72fc2a6ebe45db00001bc7
ppresto <ppresto@hashicorp.com>
2022-05-31 16:36:57 -0700

    3

--- aws_eks/templates/client-systemd.sh
+++ aws_eks/templates/client-systemd.sh
@@ -19,14 +19,14 @@ cp /root/.func-e/versions/1.20.2/bin/envoy /usr/local/bin
 envoy --version
 
 ### Install Docker, docker-compose
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update -y
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+#
+#apt-get update -y
+#apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 ### Install fake-service
 mkdir -p /opt/consul/fake-service/central_config
