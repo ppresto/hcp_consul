a599d726d8ffc35b31f6c8ae2d5f1b5f37c3322d
ppresto <ppresto@hashicorp.com>
2022-05-31 16:38:22 -0700

    4

--- aws_eks/templates/client-systemd.sh
+++ aws_eks/templates/client-systemd.sh
@@ -2,7 +2,6 @@
 
 # stored in: /var/lib/cloud/instances/instance-id/user-data.txt
 # logged at: /var/log/cloud-init-output.log
-
 CONFIG_FILE_64="${CONSUL_CONFIG_FILE}"
 CONSUL_CA=$(echo ${CONSUL_CA_FILE}| base64 -d)
 
@@ -17,16 +16,6 @@ func-e versions -all
 func-e use 1.20.2
 cp /root/.func-e/versions/1.20.2/bin/envoy /usr/local/bin
 envoy --version
-
-### Install Docker, docker-compose
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-#
-#apt-get update -y
-#apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 ### Install fake-service
 mkdir -p /opt/consul/fake-service/central_config
